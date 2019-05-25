76a8af691dc70b9e47111c2d4e79e0d2322675f0
Eliot Howes <ejhowes@gmail.com>
2019-05-25 17:22:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,6 +21,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(deli_line)
+  deli_line = ["Avi", "Spencer"]
   if deli_line.empty?
     puts "There is nobody waiting to be served!"
   else
